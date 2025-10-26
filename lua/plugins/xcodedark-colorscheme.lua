8a9589514b54ae6d3873c4db5195a2167abac6a5
Gábor Librecz <kuglee@gmail.com>
2025-10-26 08:32:43 +0100

    Xcodedark-colorscheme: fix match highlighting in Telescope

--- lua/plugins/xcodedark-colorscheme.lua
+++ lua/plugins/xcodedark-colorscheme.lua
@@ -10,5 +10,6 @@ return {
 
     vim.cmd.colorscheme "xcodedark"
     vim.opt.guicursor:append "a:blinkon0"
+    vim.api.nvim_set_hl(0, "TelescopePreviewLine", { bg = "#636f83", bold = true })
   end,
 }
