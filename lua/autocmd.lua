d7b1ac75c5da40ff0de8d32f80ee6e6766c4baad
Gábor Librecz <kuglee@gmail.com>
2025-11-01 21:36:12 +0100

    autocmd: refactor clearing last command message

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,19 +1,3 @@
--- remove padding around neovim instance
-vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-  callback = function()
-    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-    if not normal.bg then
-      return
-    end
-    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
-  end,
-})
-vim.api.nvim_create_autocmd("UILeave", {
-  callback = function()
-    io.write "\027]111\027\\"
-  end,
-})
-
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
@@ -26,10 +10,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- hide last command
-function Empty_message()
+-- clear last command message
+local function clear_last_command_message()
   if vim.api.nvim_get_mode().mode ~= "c" then
     vim.api.nvim_echo({ { "" } }, false, {})
   end
 end
-vim.cmd [[autocmd CmdlineLeave * lua vim.defer_fn(Empty_message, 5000)]]
+
+vim.api.nvim_create_autocmd("CmdlineLeave", {
+  pattern = "*",
+  callback = function()
+    vim.defer_fn(clear_last_command_message, 5000)
+  end,
+})
