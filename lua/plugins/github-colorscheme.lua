af85a4a98124574d30f840a748db0151670f3077
Gábor Librecz <kuglee@gmail.com>
2025-11-01 21:33:57 +0100

    github-colorscheme: fix setup

--- lua/plugins/github-colorscheme.lua
+++ lua/plugins/github-colorscheme.lua
@@ -1,7 +1,3 @@
 vim.pack.add { "https://github.com/projekt0n/github-nvim-theme" }
 
-require("github-theme").setup {
-  styles = {
-    comments = { italic = true },
-  },
-}
+require("github-theme").setup()
