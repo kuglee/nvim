c9fcaff719a8f470238e33fcf16202afdf9415b2
Gábor Librecz <kuglee@gmail.com>
2025-11-11 17:25:03 +0100

    Autocmd: always update quickfix list to make jumping between diagnostics work

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,3 +1,10 @@
+-- update quickfix list to make jumping between diagnostics work
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+  callback = function()
+    vim.diagnostic.setqflist { open = false }
+  end,
+})
+
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
