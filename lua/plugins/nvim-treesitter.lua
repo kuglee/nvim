9a968d3e8a08f2c65aab7e4b955a639d377fef17
Gábor Librecz <kuglee@gmail.com>
2025-11-11 23:11:00 +0100

    nvim-treesitter: use the main branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,29 +1,56 @@
+-- from: https://www.reddit.com/r/neovim/comments/1ou68ds/nvimtreesitter_main_rewrite_did_i_do_this_right
+
 vim.pack.add {
-  "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/windwp/nvim-ts-autotag",
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 }
 
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
-  once = true,
-  callback = function()
-    require("nvim-treesitter.configs").setup {
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotag = { enable = false },
-      ensure_installed = {
-        "json",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "lua",
-        "gitignore",
-        "swift",
-        "elm",
-        "javascript",
-        "typescript",
-        "php",
-      },
-      auto_install = true,
-    }
+local ts = require "nvim-treesitter"
+local augroup = vim.api.nvim_create_augroup("config.treesitter", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  pattern = "*",
+  callback = function(event)
+    local filetype = event.match
+    local lang = vim.treesitter.language.get_lang(filetype)
+
+    if not lang then
+      return
+    end
+
+    local is_installed = vim.treesitter.language.add(lang)
+
+    if not is_installed then
+      local available_langs = ts.get_available()
+      local is_available = vim.tbl_contains(available_langs, lang)
+
+      if is_available then
+        vim.notify("Installing Tree-sitter parser for " .. lang .. "...", vim.log.levels.INFO)
+        vim.schedule(function()
+          ts.install { lang }
+        end)
+      else
+        return
+      end
+    end
+
+    local ok = pcall(vim.treesitter.start, event.buf, lang)
+    if not ok then
+      return
+    end
+
+    vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end,
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  group = augroup,
+  pattern = { "nvim-treesitter" },
+  callback = function(_)
+    vim.notify("Updating treesitter parsers...", vim.log.levels.INFO)
+    vim.schedule(function()
+      ts.update(nil, { summary = true })
+    end)
   end,
 })
