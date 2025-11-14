288d643c343d99bc4c7608cb34c50873ab54d62e
Gábor Librecz <kuglee@gmail.com>
2025-11-14 20:36:18 +0100

    Remove auto-session plugin

--- lua/plugins/auto-session.lua
+++
@@ -1,6 +0,0 @@
-vim.pack.add { "https://github.com/rmagatti/auto-session" }
-
-require("auto-session").setup()
-
-vim.o.sessionoptions =
-  "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
