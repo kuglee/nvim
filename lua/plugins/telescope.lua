0df1bd71ab29a731c17584767168cdab77ab641c
Gábor Librecz <kuglee@gmail.com>
2025-11-17 17:54:51 +0100

    Telescope: use the main branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 vim.pack.add {
-  { src = "https://github.com/nvim-telescope/telescope.nvim", version = "0.1.x" },
+  "https://github.com/nvim-telescope/telescope.nvim",
   "https://github.com/nvim-lua/plenary.nvim",
 }
 
