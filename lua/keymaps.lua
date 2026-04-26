6cb2adf0d1a14f3d9c814dd0cc23ded8fcd81a14
Gábor Librecz <kuglee@gmail.com>
2026-04-26 09:49:22 +0200

    Add keybinding for qfix ufocused open

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -93,3 +93,21 @@ for _, op in ipairs { "x", "X", "d", "D", "c", "s" } do
   vim.keymap.set({ "n", "v" }, op, '"z' .. op)
 end
 vim.keymap.set("n", "dd", '"zdd') -- no dd in visual mode
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function()
+    vim.keymap.set('n', '<leader>p', function()
+      local win = vim.api.nvim_get_current_win()
+      vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes('<CR>', true, false, true),
+        'n',
+        false
+      )
+      vim.schedule(function()
+        vim.api.nvim_set_current_win(win)
+      end)
+    end, { buffer = true, silent = true })
+  end,
+})
+
