a007f31036b195ca3249afaa8f581138ebfefa8b
Gábor Librecz <kuglee@gmail.com>
2026-05-28 19:17:44 +0200

    Fugitive: change split height
    WIP

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,1 +1,9 @@
 vim.pack.add { "https://github.com/tpope/vim-fugitive" }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fugitive",
+  callback = function()
+    vim.cmd "resize 15"
+    vim.opt_local.winfixheight = true
+  end,
+})
